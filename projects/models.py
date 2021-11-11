231accc47a305fec8c1a82360907a0b446ed693f
baijuka <baijuka@yahoo.com>
2021-11-11 00:49:54 +0000

    Project model created

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+import uuid
 
 # Create your models here.
+
+class Project(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank = True)
+    #owner
+    #featured_image
+    demo_link = models.CharField(max_length=1000, null=True, blank=True)
+    source_link = models.CharField(max_length=1000, null=True, blank=True)
+    vote_total = models.IntegerField(default=0)
+    vote_ratio = models.IntegerField(default=0)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
+
+    def __str__(self):
+        return self.title
--- projects/views.py
+++ projects/views.py
@@ -4,8 +4,55 @@ from . import views
 
 # Create your views here.
 
+projectlist = [
+    {'id':'1',
+     'title':'Ecommerce Website',
+     'description':'Fully functional ecommerce website'
+     },
+    {'id':'2',
+     'title':'Hotel Management System',
+     'description':'Hotel Management System Development for Holiday Inn, London'
+     },
+    {'id':'3',
+     'title':'Patient Appointment Booking App',
+     'description':'Patient appointment booking site for a Glan-Usk Surgery'
+     },
+    {'id':'4',
+     'title':'Online Ticket Booking System',
+     'description':'Ticket booking site development'
+     }
+    ]
+
+
 def projects(request):
-    return render(request, 'projects/projects.html')
+
+    context = {'projects':projectlist}
+    return render(request, 'projects/projects.html', context)
+
 
 def project(request, pk):
-    return render(request, 'projects/single-project.html')
+
+    projectlist = [
+        {'id':'1',
+        'title':'Ecommerce Website',
+        'description':'Fully functional ecommerce website'
+        },
+        {'id':'2',
+        'title':'Hotel Management System',
+        'description':'Hotel Management System Development for Holiday Inn, London'
+        },
+        {'id':'3',
+        'title':'Patient Appointment Booking App',
+        'description':'Patient appointment booking site for a Glan-Usk Surgery'
+        },
+        {'id':'4',
+        'title':'Online Ticket Booking System',
+        'description':'Ticket booking site development'
+        }]
+    
+    projectObject = None
+
+    for i in projectlist:
+        if i['id']==str(pk):
+            projectObject = i
+    return render(request, 'projects/single-project.html', {'project':projectObject})
