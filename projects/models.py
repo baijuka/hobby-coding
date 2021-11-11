a63d1fda96c9ac7d5e28a6c70518dc3ae0630f5b
baijuka <baijuka@yahoo.com>
2021-11-11 09:35:02 +0000

    Add Review and Tag models

--- projects/models.py
+++ projects/models.py
@@ -3,9 +3,19 @@ import uuid
 
 # Create your models here.
 
+class Tag(models.Model):
+    name = models.CharField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+
+    def __str__(self):
+        return self.name
+
+
 class Project(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField(null=True, blank = True)
+    tags = models.ManyToManyField('Tag', blank=True)
     #owner
     #featured_image
     demo_link = models.CharField(max_length=1000, null=True, blank=True)
@@ -16,3 +26,19 @@ class Project(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Review(models.Model):
+    VOTE_TYPE = (('up','up'),('down','down'))
+
+    project = models.ForeignKey(Project,on_delete=models.CASCADE, null=True, blank=True)
+    body = models.TextField(null=True, blank=True)
+    value = models.CharField(max_length=50,choices=VOTE_TYPE)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+
+    def __str__(self):
+        return self.value
+
+
--- projects/views.py
+++ projects/views.py
@@ -1,58 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from . import views
+from .models import Project
 
 # Create your views here.
 
-projectlist = [
-    {'id':'1',
-     'title':'Ecommerce Website',
-     'description':'Fully functional ecommerce website'
-     },
-    {'id':'2',
-     'title':'Hotel Management System',
-     'description':'Hotel Management System Development for Holiday Inn, London'
-     },
-    {'id':'3',
-     'title':'Patient Appointment Booking App',
-     'description':'Patient appointment booking site for a Glan-Usk Surgery'
-     },
-    {'id':'4',
-     'title':'Online Ticket Booking System',
-     'description':'Ticket booking site development'
-     }
-    ]
-
-
 def projects(request):
 
-    context = {'projects':projectlist}
+    projects = Project.objects.all()
+    context = {'projects': projects}
     return render(request, 'projects/projects.html', context)
 
 
 def project(request, pk):
 
-    projectlist = [
-        {'id':'1',
-        'title':'Ecommerce Website',
-        'description':'Fully functional ecommerce website'
-        },
-        {'id':'2',
-        'title':'Hotel Management System',
-        'description':'Hotel Management System Development for Holiday Inn, London'
-        },
-        {'id':'3',
-        'title':'Patient Appointment Booking App',
-        'description':'Patient appointment booking site for a Glan-Usk Surgery'
-        },
-        {'id':'4',
-        'title':'Online Ticket Booking System',
-        'description':'Ticket booking site development'
-        }]
-    
-    projectObject = None
+    projectObject = Project.objects.get(id=pk)
 
-    for i in projectlist:
-        if i['id']==str(pk):
-            projectObject = i
     return render(request, 'projects/single-project.html', {'project':projectObject})
