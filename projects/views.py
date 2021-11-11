925b2ee7ae620e3ac8fd6416e7a0765ac9d57b79
baijuka <baijuka@yahoo.com>
2021-11-11 16:37:28 +0000

    Added create and update functionality to forms

--- projects/views.py
+++ projects/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from . import views
 from .models import Project
+from .forms import ProjectForm
 
 # Create your views here.
 
@@ -19,3 +20,30 @@ def project(request, pk):
     reviews = projectObject.review_set.all()
     context = {'project':projectObject, 'tags':tags, 'reviews':reviews}
     return render(request, 'projects/single-project.html', context)
+
+
+def createProject(request):
+    form = ProjectForm()
+
+    if request.method == 'POST':
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('projects')
+
+    context = {'form': form}
+    return render(request, 'projects/project-form.html', context)
+
+
+def updateProject(request,pk):
+    project = Project.objects.get(id=pk)
+    form = ProjectForm(instance=project)
+
+    if request.method == 'POST':
+        form = ProjectForm(request.POST, instance=project)
+        if form.is_valid():
+            form.save()
+            return redirect('projects')
+
+    context = {'form':form}
+    return render(request, 'projects/project-form.html', context)
