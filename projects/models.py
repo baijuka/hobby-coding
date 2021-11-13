649b6338124598e02cb1c61d956a2d8c0acd4fe8
baijuka <baijuka@yahoo.com>
2021-11-13 16:31:40 +0000

    Default image attribute added

--- projects/models.py
+++ projects/models.py
@@ -17,7 +17,7 @@ class Project(models.Model):
     description = models.TextField(null=True, blank = True)
     tags = models.ManyToManyField('Tag', blank=True)
     #owner
-    featured_image = models.ImageField(null=True, blank=True)
+    featured_image = models.ImageField(null=True, blank=True, default='default.jpg')
     demo_link = models.CharField(max_length=1000, null=True, blank=True)
     source_link = models.CharField(max_length=1000, null=True, blank=True)
     vote_total = models.IntegerField(default=0)
